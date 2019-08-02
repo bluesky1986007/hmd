07d529c24f74a1eed74f20b730ad536bd2acb827
陆祎 <bluesky1986007@sina.cn>
2019-08-02 18:29:18 +0800

    update
    update

---
+++ api_web_core/src/main/java/common/utils/UrlsOfPre.java
@@ -0,0 +1,22 @@
+package common.utils;
+
+public enum UrlsOfPre {
+
+
+	/**
+	 * 后台管理系统url
+	 * @author luyi
+	 *
+	 */
+BackGroundSystem {
+		@Override
+		public String getUrl(){
+			String url = "";
+			return url;
+		}
+	};
+
+	public abstract String getUrl();
+
+}
+
